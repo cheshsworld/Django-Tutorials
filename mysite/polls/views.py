498a4e2ca0fe3b04224b3cce9dbcf0a52c4fa3d0
cheshsworld <94447348+cheshsworld@users.noreply.github.com>
2022-02-01 17:51:16 +0800

    commits

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,11 +3,14 @@
 # Create your views here.
 from urllib import response
 from django.http import HttpResponse
+from django.template import loader
 
-from mysite.polls.models import Question
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
